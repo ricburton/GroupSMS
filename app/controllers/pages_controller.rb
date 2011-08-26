2cb98b034eb274437830a7d7e16ab0789750ced1
Richard <burtonic@gmail.com>
2011-08-27 00:17:19 +0100

    added bootstrap css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Welcome"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title = "GroupTxt"
+    if @title.nil?
+      @title = base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end

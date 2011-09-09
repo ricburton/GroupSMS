9d017ce2752f55ced7b534a33ae8cc34b4cd3fe3
Richard <burtonic@gmail.com>
2011-09-09 12:08:23 +0100

    del

--- db/migrate/20110904230256_add_status_to_memberships_and_convert_string_to_text_in_messages.rb
+++
@@ -1,8 +0,0 @@
-class AddStatusToMembershipsAndConvertStringToTextInMessages < ActiveRecord::Migration
-  def self.up
-    add_column :memberships, :status, :string
-  end
-
-  def self.down
-  end
-end

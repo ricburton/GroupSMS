6c562dd893d1632c8cc8375a0937947852ddb9e5
Richard <burtonic@gmail.com>
2011-09-16 20:55:22 +0100

    love

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -137,4 +137,14 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+  def authenticate 
+    deny_access unless signed_in?
+  end
+  
+  def correct_user 
+    @user = User.find(params[:id])
+    redirect_to root_path unless current_user?(@user)
+  end
+
 end
